27655cbbd7fad4118156930b08d0667cd4d82ced
Steff Beckers <steff@steffbeckers.eu>
2022-11-01 00:19:30 +0100

    OAuthService injected in app

--- apps/dev-app/src/app/app.component.ts
+++ apps/dev-app/src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OAuthService } from 'angular-oauth2-oidc';
 
 @Component({
   selector: 'angular-oauth2-oidc-oauthservice-di-in-library-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'dev-app';
+export class AppComponent implements OnInit {
+  constructor(public oAuthService: OAuthService) {
+    // https://github.com/manfredsteyer/angular-oauth2-oidc#logging-in
+    this.oAuthService.configure({
+      // Url of the Identity Provider
+      issuer: 'https://idsvr4.azurewebsites.net',
+
+      // URL of the SPA to redirect the user to after login
+      redirectUri: window.location.origin + '/index.html',
+
+      // The SPA's id. The SPA is registerd with this id at the auth-server
+      // clientId: 'server.code',
+      clientId: 'spa',
+
+      // Just needed if your auth server demands a secret. In general, this
+      // is a sign that the auth server is not configured with SPAs in mind
+      // and it might not enforce further best practices vital for security
+      // such applications.
+      // dummyClientSecret: 'secret',
+      responseType: 'code',
+
+      // set the scope for the permissions the client should request
+      // The first four are defined by OIDC.
+      // Important: Request offline_access to get a refresh token
+      // The api scope is a usecase specific one
+      scope: 'openid profile email offline_access api',
+
+      showDebugInformation: true
+    });
+  }
+
+  async ngOnInit() {
+    await this.oAuthService.loadDiscoveryDocumentAndTryLogin();
+  }
+
+  login() {
+    this.oAuthService.initLoginFlow();
+  }
+
+  logout() {
+    this.oAuthService.logOut();
+  }
 }
--- apps/dev-app/src/app/app.module.ts
+++ apps/dev-app/src/app/app.module.ts
@@ -1,12 +1,17 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { OAuthModule } from 'angular-oauth2-oidc';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    OAuthModule.forRoot()
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
