d067c2d9db76fce5a32405d81929fa43769419b2
Steff Beckers <steff@steffbeckers.eu>
2022-10-31 23:53:27 +0100

    Initial commit

---
+++ apps/dev-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'angular-oauth2-oidc-oauthservice-di-in-library-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'dev-app';
+}
